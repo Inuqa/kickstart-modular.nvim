e7dc5ca26991a8bb499fd1b739f477ebdd9ddaeb
Sebastian Rojas <seba.nrc@gmail.com>
2024-11-07 11:26:48 -0300

    Configure nul ls

--- lua/custom/plugins/none_ls.lua
+++ lua/custom/plugins/none_ls.lua
@@ -1,11 +1,14 @@
 return {
   'nvimtools/none-ls.nvim',
   config = function()
-    local null_ls = require("null-ls")
+    local null_ls = require 'null-ls'
     null_ls.setup {
       sources = {
-        null_ls.builtins.diagnostics.rubocop
-      }
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.formatting.stylua,
+      },
     }
-  end
+
+    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+  end,
 }
