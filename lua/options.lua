6fda8f2cb6f11122d765999daec58fa8b7e3f577
Sebastian Rojas <seba.nrc@gmail.com>
2024-11-22 23:01:16 -0300

    Add new options

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,8 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = ''
@@ -24,6 +25,9 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.breakindent = true
 
 -- Save undo history
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
@@ -34,7 +38,7 @@ vim.opt.smartcase = true
 vim.opt.signcolumn = 'yes'
 
 -- Decrease update time
-vim.opt.updatetime = 250
+vim.opt.updatetime = 50
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
@@ -57,7 +61,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 8
 
 vim.opt.fillchars = { fold = " " }
 vim.opt.foldmethod = "indent"
@@ -68,5 +72,13 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smarttab = true
 vim.opt.expandtab = true
+vim.opt.softtabstop = 2
 
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+vim.opt.wrap = false
+vim.opt.smartindent = true
 -- vim: ts=2 sts=2 sw=2 et
